ac13f4abe63ad302799c0d47f935d5a3fcbf6c78
progyu <rocket7101@gmail.com>
2020-11-03 17:53:19 +0900

    feat: Drawing a line

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const canvas = document.getElementById('jsCanvas')
+const ctx = canvas.getContext('2d')
+
+canvas.width = 700
+canvas.height = 700
+
+ctx.strokeStyle = '#2c2c2c'
+ctx.lineWidth = 2.5
 
 let painting = false
 
-function stopPainting() {
-  painting = false
-}
+const stopPainting = () => (painting = false)
 
-function onMouseMove(event) {
+const startPainting = () => (painting = true)
+
+const onMouseMove = event => {
   const { offsetX, offsetY } = event
-}
 
-function onMouseDown(event) {
-  painting = true
-}
-
-function onMouseUp(event) {
-  stopPainting()
+  if (!painting) {
+    ctx.beginPath()
+    ctx.moveTo(offsetX, offsetY)
+  } else {
+    ctx.lineTo(offsetX, offsetY)
+    ctx.stroke()
+  }
 }
 
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove)
-  canvas.addEventListener('mousedown', onMouseDown)
-  canvas.addEventListener('mouseup', onMouseUp)
+  canvas.addEventListener('mousedown', startPainting)
+  canvas.addEventListener('mouseup', stopPainting)
   canvas.addEventListener('mouseleave', stopPainting)
 }
