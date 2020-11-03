4d538d8e24e61e53813a029e1734250bc36fe62a
progyu <rocket7101@gmail.com>
2020-11-04 01:34:57 +0900

    feat: save image

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d')
 const colors = document.getElementById('jsColors')
 const range = document.getElementById('jsRange')
 const mode = document.getElementById('jsMode')
+const saveBtn = document.getElementById('jsSave')
 
 const INITIAL_COLOR = '#2c2c2c'
 const CANVAS_SIZE = 700
@@ -10,6 +11,8 @@ const CANVAS_SIZE = 700
 canvas.width = CANVAS_SIZE
 canvas.height = CANVAS_SIZE
 
+ctx.fillStyle = 'white'
+ctx.fillRect(0, 0, canvas.width, canvas.height)
 ctx.strokeStyle = INITIAL_COLOR
 ctx.fillStyle = INITIAL_COLOR
 ctx.lineWidth = 2.5
@@ -56,12 +59,25 @@ const handleCanvasClick = () => {
   }
 }
 
+// 우클릭 방지
+const handleRigthClick = event => {
+  event.preventDefault()
+}
+
+const handleSaveBtnClick = event => {
+  const link = document.createElement('a')
+  link.href = canvas.toDataURL()
+  link.download = 'PaintJS[🖌]'
+  link.click()
+}
+
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove)
   canvas.addEventListener('mousedown', startPainting)
   canvas.addEventListener('mouseup', stopPainting)
   canvas.addEventListener('mouseleave', stopPainting)
   canvas.addEventListener('click', handleCanvasClick)
+  canvas.addEventListener('contextmenu', handleRigthClick)
 }
 
 colors.addEventListener('click', handleColorClick)
@@ -72,4 +88,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener('click', handleModeClick)
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener('click', handleSaveBtnClick)
 }
